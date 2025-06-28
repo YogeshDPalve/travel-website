ebc6c98d666c3110ba0dca25847e4fb140e64d28
Yogesh Palve <dnyyogesh@gmail.com>
2025-06-28 17:49:54 +0530

    new pages and components created and trip submit form created

---
+++ app/trips/new/page.tsx
@@ -0,0 +1,79 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+
+const NewTrip = () => {
+  return (
+    <div className="max-w-md mx-auto mt-12">
+      <Card>
+        <CardHeader>New Trip</CardHeader>
+        <CardContent>
+          <form className="space-y-4">
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Title
+              </label>
+              <input
+                type="text"
+                name="title"
+                required
+                placeholder="Japan Trip...."
+                className={cn(
+                  "w-full border border-gray-300 px-4 py-2",
+                  "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                )}
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Description
+              </label>
+              <textarea
+                required
+                name="description"
+                placeholder="Trip Description"
+                className={cn(
+                  "w-full border border-gray-300 px-4 py-2",
+                  "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                )}
+              />
+            </div>
+            <div className="flex items-center gap-4 justify-between">
+              <div className="w-full">
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                  Start Date
+                </label>
+                <input
+                  type="date"
+                  name="startDate"
+                  className={cn(
+                    "w-full border border-gray-300 px-4 py-2",
+                    "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 "
+                  )}
+                />
+              </div>
+              <div className="w-full">
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                  End Date
+                </label>
+                <input
+                  type="date"
+                  name="endDate"
+                  className={cn(
+                    "w-full border border-gray-300 px-4 py-2",
+                    "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  )}
+                />
+              </div>
+            </div>
+            <Button type="submit" className="w-full">
+              Create Trip
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default NewTrip;
